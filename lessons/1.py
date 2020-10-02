1b140a06f0a1589ea31e71cfac615c180c551486
yaizkazani <61969939+yaizkazani@users.noreply.github.com>
2020-10-02 13:55:34 +0300

    Update 1.py

--- lessons/1.py
+++ lessons/1.py
@@ -1,7 +1,42 @@
-a = input("Enter a\n")
-a = int(a)
-b = int(input("Enter b\n"))
-print("a + b = ", a + b)
-print("a - b = ", a - b)
-print("a * b = ", a * b)
-print("a / b = ", int(a / b))
+# print(type("hey"))
+# print(type('hello'))
+# print(type(4))
+# b = 4
+# a = 3 + 5
+# print(a)
+# print(3 + 5)
+# a = 5
+# print("Hello " + "world!" + " " + str(a))
+#print("1+2= ", 1 + 2)
+# end = "\n"
+# print("Hello", "world", sep=" ", end="\n\n")
+# print("Hello", "world", sep="+")
+# print("Secondprint")
+# ==============================
+
+# a = input("Enter a\n")
+# a = int(a)
+# b = int(input("Enter b\n"))
+# print("a + b = ", a + b)
+# print("a - b = ", a - b)
+# print("a * b = ", a * b)
+# print("a / b = ", a / b)
+
+# ==============================
+
+# a = int(input("Enter a\n"))
+# b = int(input("Enter b\n"))
+#
+# print("a // b = ", a // b)
+# print("a % b = ", a % b)
+
+# Augmented assignment statements
+
+# s = 12345
+# print(id(s))
+# s *= 2
+# print(id(s))
+# print(s)
+# print(12345)
+# print(5)
+
