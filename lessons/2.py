b259b29d4004422220b6c50893e1e221980cbd00
yaizkazani <61969939+yaizkazani@users.noreply.github.com>
2020-10-05 16:19:56 +0300

    Update 2.py

--- lessons/2.py
+++ lessons/2.py
@@ -1,13 +1,24 @@
-print("1 == 2", 1 == 2)
-print("1 == 1", 1 == 1)
-print("True == False", True == False)
-print("False == False", False == False)
-print("True != False", True != False)
-print("3 > 4", 3 > 4)
-print("3 <= 4", 3 <= 4)
-print("3 != 4", 3 != 4)
-print("3 < 4 <= 5", 3 < 4 <= 5)
+# s = "123.4"
+# print(type(s))
+# print(float(s))
+# print(round(float(s)))
+# print(int(float(s)))
 
+
+
+# print("1 == 2", 1 == 2)
+# print("1 == 1", 1 == 1)
+# print("True == False", True == False)
+# print("False == False", False == False)
+# print("True != False", True != False)
+# print("3 > 4", 3 > 4)
+# print("3 <= 4", 3 <= 4)
+# print("3 != 4", 3 != 4)
+# print("3 < 4 <= 5", 3 < 4 <= 5)
+# print((3 < 4) and (4 <= 5))
+# print((True and False) == False)
+# print((True and True) == True)
+# print((False and False) == False)
 
 #
 # print("1 == 2 and 1 == 1", 1 == 2 and 1 == 1)
@@ -15,14 +26,34 @@ print("3 < 4 <= 5", 3 < 4 <= 5)
 # print("not True == False", not True == False)
 # print("not 3 > 4 and 3 <= 4", not 3 > 4 and 3 <= 4 )
 
+# print(len("123456"))
+# print(len(str(1234)))
 
+# s = "123"
+# b = "1234"
+# c = "123"
+#
+# print(not(s == b) and not(s == c))
+# print(not(s == b) or (s == b))
+#
+# print(((s == b) and (s == c)) or (s == c))
 
 
 # a, b = int(input()), int(input())
 #
 # if a > b:
 #     print("a > b")
+#     print('if worked')
 # elif a == b:
 #     print("a = b")
+# elif a >= b:
+#     print("2nd elif")
 # else:
 #     print("a < b")
+
+
+
+# i = 99
+# while i > 0:
+#     print(i)
+#     i -= 1
